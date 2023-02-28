ba5d650b30c20d7e227e51a3a8eca1191246376a
theuagadugu <theuagadugu@gmail.com>
2023-02-28 17:45:32 +0100

    Afegeix pàgines d'ofertes de feines individuals.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify
 import MySQLdb
 import ssl
 import os
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 print("hola")
 
 app=Flask(__name__)
@@ -17,7 +17,13 @@ def helloWorld():
 def list_jobs():
 	return(jsonify(load_jobs_from_db()))
 
-
-
+@app.route("/job/<id>") #la sintaxi <>indica qeu id és un paràmetre de la funció
+def show_job(id):
+	try:
+		job=load_job_from_db(id)
+	except:
+		return "Job not found 404"
+	return render_template("jobpage.html",job=job)
+	
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -10,20 +10,41 @@ password= os.getenv("DB_PASSWORD")
 database= os.getenv("DB_DATABASE")
 
 
+
+
 def load_jobs_from_db():
-	connection = MySQLdb.connect(
+	"""
+	Returns a list of jobs (list of dictionarys)
+	"""
+	with MySQLdb.connect(
 		host= host,
 		user=user,
 		passwd= password,
 		db= database,
-		ssl={'ssl': {'cert_reqs': ssl.CERT_NONE}})	
-	c=connection.cursor()
-	c.execute("select * from jobs")
-	result=list(c.fetchall())
-	keys=[i[0] for i in c.description]
-	a=[dict(zip(keys,i)) for i in result]
-	return [dict(zip(keys,i)) for i in result]
+		ssl={'ssl': {'cert_reqs': ssl.CERT_NONE}})	as connection:
+		c=connection.cursor()
+		c.execute("select * from jobs")
+		result=list(c.fetchall())
+		keys=[i[0] for i in c.description]
+		return [dict(zip(keys,i)) for i in result]
+
+def load_job_from_db(id):
+	"""
+	Returns a single dictionary with a row info
+	"""
+	with MySQLdb.connect(
+		host= host,
+		user=user,
+		passwd= password,
+		db= database,
+		ssl={'ssl': {'cert_reqs': ssl.CERT_NONE}})	as connection:
+		c=connection.cursor()
+		c.execute(f"select * from jobs where id={id}")
+		r=c.fetchall()
+		keys=[i[0] for i in c.description]
+		return [dict(zip(keys,i)) for i in r][0]
 
 if __name__ == '__main__':
 	#print(os.getenv("HOST"))
-	print(load_jobs_from_db(), type(load_jobs_from_db()))
+	#print(load_jobs_from_db(), type(load_jobs_from_db()))
+	print(load_job_from_db(2))
