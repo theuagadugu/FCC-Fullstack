327756180f40afc414a770fb018a0db3b47182a9
theuagadugu <theuagadugu@gmail.com>
2023-03-01 19:00:16 +0100

    Afegeix nous elements a la data base des de la web.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import MySQLdb
 import ssl
 import os
-from database import load_jobs_from_db, load_job_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 print("hola")
 
 app=Flask(__name__)
@@ -24,6 +24,18 @@ def show_job(id):
 	except:
 		return "Job not found 404"
 	return render_template("jobpage.html",job=job)
-	
+
+@app.route("/job/<id>/apply")
+def apply_to_job(id):
+	"""
+	Hi ha una altra manera de fer-ho amb el tag method="post" desdel form del html
+	i utilitzant request.form aquí. (4:03:00)
+	"""
+	#return render_template("application_submited.html",application=jsonify(request.args))
+	data=request.args
+	add_application_to_db(id,data)
+	return render_template("application_submited.html",application=data,job=load_job_from_db(id))
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -44,7 +44,47 @@ def load_job_from_db(id):
 		keys=[i[0] for i in c.description]
 		return [dict(zip(keys,i)) for i in r][0]
 
+
+def get_applications_table_from_db():
+	"""
+	Crea la taula d'aplicacions. No executar mai!!!
+	"""
+
+	with MySQLdb.connect(
+		host= host,
+		user=user,
+		passwd= password,
+		db= database,
+		ssl={'ssl': {'cert_reqs': ssl.CERT_NONE}})	as connection:
+		c=connection.cursor()
+		c.execute("select * from APPLICATIONS;")
+		result=list(c.fetchall())
+		return result
+
+
+def add_application_to_db(job_id,data):
+	with MySQLdb.connect(
+		host= host,
+		user=user,
+		passwd= password,
+		db= database,
+		ssl={'ssl': {'cert_reqs': ssl.CERT_NONE}})	as connection:
+		c=connection.cursor()
+		sentence=f"INSERT INTO APPLICATIONS (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({job_id}, '{data['full_name']}', '{data['email']}', '{data['linkedin']}', '{data['education']}', '{data['work_experience']}', '{data['url']}')"
+		c.execute(sentence)
+		result=connection.commit()
+		print(result)
+
+
+#ImmutableMultiDict([('full_name', 'nom complet2'), ('email', 'correu'), ('linkedin', 'linked'), ('education', 'educacio i tal '), ('work_experience', 'si he cotitzat'), ('url', 'url')])
+
 if __name__ == '__main__':
+	"""
+	data={'full_name': 'nom complet3', 'email': 'correu', 'linkedin': 'linked', 'education': 'educacio i tal ', 'work_experience': 'si he cotitzat', 'url': 'url'}
+	add_application_to_db(1,data)
+	print("DATABASE:")
+	print(get_applications_table_from_db())
+	"""
 	#print(os.getenv("HOST"))
 	#print(load_jobs_from_db(), type(load_jobs_from_db()))
-	print(load_job_from_db(2))
+	#print(load_job_from_db(2))
